c8fa7dc7244b9ac725a3b6c29e50d1651cbcf6de
Kurt Landrus <klandrus@gmail.com>
2016-12-16 00:37:36 -0500

    end chapt 2

--- Main.elm
+++ Main.elm
@@ -1,27 +1,58 @@
 
 import Html exposing (..)
-import Html.CssHelpers exposing (..)
+import Html.Events exposing (onClick)
 import Html.Attributes exposing (..)
+import Html.CssHelpers exposing (..)
 import MyCss exposing (..)
 
 { id, class, classList } =
     Html.CssHelpers.withNamespace "home"
 
+initialModel =
+  { photos =
+    [ { url = "1.jpeg" }
+    , { url = "2.jpeg" }
+    , { url = "3.jpeg" }
+    ]
+  , selectedUrl = "1.jpeg"
+  }
 
 main =
-  view "no model yet"
+  Html.beginnerProgram
+    { model = initialModel
+    , view = view
+    , update = update
+    }
 
+-- Update
+
+update msg model =
+  if msg.operation == "SELECT_PHOTO" then
+    { model | selectedUrl = msg.data }
+  else
+    model
+
+-- View
 
 view model =
   div [ class [Content] ]
       [ h1 [] [ text "Photo Grove" ]
       , div [ id Thumbnails ]
-          [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-          , img [ src "http://elm-in-action.com/2.jpeg"] []
-          , img [ src "http://elm-in-action.com/3.jpeg"] []
-          ]
+          (List.map (viewThumbnail model.selectedUrl) model.photos)
+      , img
+          [class ["large"]
+          , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+          ] []
       ]
 
+viewThumbnail selectedUrl thumbnail =
+  img [ src (urlPrefix ++ thumbnail.url)
+  , classList [ ( "selcted", selectedUrl == thumbnail.url ) ]
+  , onClick { operation = "SELECT_PHOTO", data = thumbnail.url }
+  ] []
+
+urlPrefix =
+  "http://elm-in-action.com/"
 
 -- navElems =
 --     [ "SPEAKERS", "SCHEDULE", "WORKSHOPS", "VENUE", "BLOG", "CONTACT" ]
