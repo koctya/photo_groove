1aa970d3056e58a6edf59edb548529e7324ac16e
Kurt Landrus <klandrus@gmail.com>
2016-12-19 11:44:09 -0500

    complete 3.2

--- Main.elm
+++ Main.elm
@@ -1,58 +1,152 @@
+module Main exposing (..)
 
+import Array exposing (Array)
 import Html exposing (..)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (..)
 import Html.CssHelpers exposing (..)
 import MyCss exposing (..)
 
+
 { id, class, classList } =
     Html.CssHelpers.withNamespace "home"
 
+
+type alias Photo =
+    { url : String }
+
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
+    , chosenSize : ThumbnailSize
+    }
+
+
+type Msg
+    = SelectByUrl String
+    | SurpriseMe
+    | SetSize ThumbnailSize
+
+
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
+
+
+initialModel : Model
 initialModel =
-  { photos =
-    [ { url = "1.jpeg" }
-    , { url = "2.jpeg" }
-    , { url = "3.jpeg" }
-    ]
-  , selectedUrl = "1.jpeg"
-  }
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    , chosenSize = Medium
+    }
+
+
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
 
 main =
-  Html.beginnerProgram
-    { model = initialModel
-    , view = view
-    , update = update
-    }
+    Html.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
+
+
+getPhotoUrl : Int -> String
+getPhotoUrl index =
+    case Array.get index photoArray of
+        Just photo ->
+            photo.url
+
+        Nothing ->
+            ""
+
+
 
 -- Update
 
+
+update : Msg -> Model -> Model
 update msg model =
-  if msg.operation == "SELECT_PHOTO" then
-    { model | selectedUrl = msg.data }
-  else
-    model
+    case msg of
+        SelectByUrl url ->
+            { model | selectedUrl = url }
+
+        SurpriseMe ->
+            { model | selectedUrl = "2.jpeg" }
+
+        SetSize size ->
+            { model | chosenSize = size }
+
+
 
 -- View
 
+
+view : Model -> Html Msg
 view model =
-  div [ class [Content] ]
-      [ h1 [] [ text "Photo Grove" ]
-      , div [ id Thumbnails ]
-          (List.map (viewThumbnail model.selectedUrl) model.photos)
-      , img
-          [class ["large"]
-          , src (urlPrefix ++ "large/" ++ model.selectedUrl)
-          ] []
-      ]
+    div [ class [ Content ] ]
+        [ h1 [] [ text "Photo Grove" ]
+        , button
+            [ onClick SurpriseMe ]
+            [ text "Surprise Me!" ]
+        , h3 [] [ text "Thumbnail Size:" ]
+        , div [ id "choose-size" ]
+            (List.map viewSizeChooser [ Small, Medium, Large ])
+        , div [ id Thumbnails, class [ (sizeToString model.chosenSize) ] ]
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , img
+            [ class [ "large" ]
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
+        ]
 
+
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumbnail =
-  img [ src (urlPrefix ++ thumbnail.url)
-  , classList [ ( "selcted", selectedUrl == thumbnail.url ) ]
-  , onClick { operation = "SELECT_PHOTO", data = thumbnail.url }
-  ] []
+    img
+        [ src (urlPrefix ++ thumbnail.url)
+        , classList [ ( "selcted", selectedUrl == thumbnail.url ) ]
+        , onClick (SelectByUrl thumbnail.url)
+        ]
+        []
 
+
+urlPrefix : String
 urlPrefix =
-  "http://elm-in-action.com/"
+    "http://elm-in-action.com/"
+
+
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+    label []
+        [ input [ type_ "radio", name "size", onClick (SetSize size) ] []
+        , text (sizeToString size)
+        ]
+
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+
+        Medium ->
+            "med"
+
+        Large ->
+            "large"
+
+
 
 -- navElems =
 --     [ "SPEAKERS", "SCHEDULE", "WORKSHOPS", "VENUE", "BLOG", "CONTACT" ]
--- MyCss.elm
+++ MyCss.elm
@@ -1,8 +1,9 @@
 module MyCss exposing (..)
 
 import Css exposing (..)
-import Css.Elements exposing (body, img, h1, a)
+import Css.Elements exposing (body, img, h1, h3, label, button, a)
 import Css.Namespace exposing (namespace)
+
 
 type CssClasses
     = NavLink
@@ -13,13 +14,16 @@ type CssClasses
 
 type CssIds
     = Thumbnails
+    | ChooseSize
+    | ActivateGroove
 
 
 css =
     (stylesheet << namespace "home")
         [ body
-            [ backgroundColor (rgb 44 44 44)
-            , color white
+            [ fontFamilies [ "Verdana" ]
+            , backgroundColor (rgb 44 44 44)
+            , color (rgb 250 250 250)
             ]
         , img
             [ border3 (px 1) solid white
@@ -45,20 +49,56 @@ css =
             [ width (px 440)
             , float left
             ]
+        , (#) ChooseSize
+            [ marginLeft (px 20)
+            , float left
+            ]
+        , (#) ActivateGroove
+            [ marginRight (px 20)
+            , marginTop (px 15)
+            , float right
+            ]
         , h1
-            [ fontFamilies  ["Verdana"]
+            [ marginBottom zero
             , color someColor
             ]
+        , h3
+            [ margin zero
+            , float left
+            ]
+        , label [ verticalAlign bottom ]
         , a
             [ textDecoration none
-
             , hover
-              [ textDecoration underline ]
-      ]
+                [ textDecoration underline ]
+            ]
+        , button
+            [ float right
+            , backgroundColor someColor
+            , border zero
+            , color (rgb 44 44 44)
+            , fontSize (px 24)
+            , cursor pointer
+            , padding2 (px 10) (px 30)
+            ]
         ]
+
+
+
+-- #thumbnails.small img { width: 50px; }
+-- #thumbnails.med img { width: 100px; }
+-- #thumbnails.large img { width: 200px; }
+-- #choose-size { float: left; margin-left: 20px; }
+-- #choose-size > span { display: inline-block; margin: 0 10px; }
+-- button:hover {
+--   background-color: white;
+-- }
+-- #thumbnails, img.large, #choose-size, h3 { margin-top: 20px; }
+
 
 someColor =
     (hex "60b5cc")
+
 
 white =
     (hex "ffffff")
